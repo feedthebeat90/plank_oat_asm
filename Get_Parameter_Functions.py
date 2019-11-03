e5a85cddd9ee747c567d1d00147564d699d6d1cc
Aja Klevs <ajaklevs@Ajas-MacBook-Pro.local>
2019-11-03 16:47:28 -0500

    cleaned a little and added comments

--- Get_Parameter_Functions.py
+++ Get_Parameter_Functions.py
@@ -5,6 +5,7 @@ from fuzzywuzzy import process
 from sklearn.base import BaseEstimator
 from itertools import product
 
+#class which makes FuzzyWuzzy act like an aklearn model
 class FuzzyWuzzy(BaseEstimator):  
     """An example of classifier"""
 
@@ -15,15 +16,13 @@ class FuzzyWuzzy(BaseEstimator):
     def fit(self, X, y=None):
 
         return self
-    
-    #assumes x is a tuple
-    def fuzwuz(x):
-        return fuzz.ratio(x[0],x[1])
         
 
     def predict_proba(self, X):
         return np.array([fuzz.ratio(x[0], x[1]) for x in X])
 
+#function that takes as input two lists of strings and outputs the most likely
+#matches using scores from the passed-in model
 def get_predictions(list1, list2, model, num_matches):
     pairs = np.array(list(product(list1,list2)))
     scores = model.predict_proba(pairs)
@@ -34,6 +33,9 @@ def get_predictions(list1, list2, model, num_matches):
     
     return truncated_df['Pairs']
 
+#the interface with the user who asks them whether the predicted matches
+#are actually a match. Returns a dictionary with keys that are the pairs
+#and values that are 1 or 0 if there is or is not a match
 def ask_about_matches(match_pairs):
     match_dct = {}
     for pair in match_pairs:
